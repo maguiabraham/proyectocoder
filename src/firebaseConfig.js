ce9724c58e424b5f6b728210abb8ea22a1bea551
maguiabraham <magui.abraham.20@gmail.com>
2021-01-27 23:44:11 -0300

    firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app"
+import "firebase/firestore"
+
+const miConfiguracion = {
+  apiKey: "AIzaSyD5QeR82wyjmE4v2odjwabjrYqXrhpmEa4",
+  authDomain: "proyecto-coder-af30c.firebaseapp.com",
+  projectId: "proyecto-coder-af30c",
+  storageBucket: "proyecto-coder-af30c.appspot.com",
+  messagingSenderId: "276191501477",
+  appId: "1:276191501477:web:45ccea220429e142df414c"
+  }
+
+ const app = firebase.initializeApp(miConfiguracion)
+ const firestore = firebase.firestore(app)
